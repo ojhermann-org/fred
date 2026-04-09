739e2239e5d875aa03073b3dbbb8168cdb9c3ce7
Otto Hermann <ojhermann@gmail.com>
2026-04-09 14:42:49 -0400

    modify RequestBuilder

--- src/request/main.py
+++ src/request/main.py
@@ -1,7 +1,8 @@
 from request.implementation.builder.source import Builder as Source
+from request.interface.builder import Builder
 
 
 class RequestBuilder:
     @staticmethod
-    def source() -> type[Source]:
+    def source() -> type[Builder]:
         return Source
--- tests/request_test/main_test.py
+++ tests/request_test/main_test.py
@@ -1,7 +1,6 @@
 import pytest
 
 from request.enum.endpoint import Endpoint
-from request.implementation.builder.source import Builder as SourceBuilder
 from request.main import RequestBuilder
 
 VALID_KEY = "a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4"
@@ -9,14 +8,15 @@ VALID_SOURCE_ID = 1
 
 
 @pytest.mark.unit_test
-def test_source_returns_source_builder_class() -> None:
-    assert RequestBuilder.source() is SourceBuilder
+def test_source_returns_a_builder_type() -> None:
+    builder_class = RequestBuilder.source()
+    instance = builder_class(api_key=VALID_KEY, source_id=VALID_SOURCE_ID)
+    assert callable(getattr(instance, "build", None))
 
 
 @pytest.mark.unit_test
 def test_source_returned_class_is_instantiable() -> None:
-    builder = RequestBuilder.source()(api_key=VALID_KEY, source_id=VALID_SOURCE_ID)
-    assert isinstance(builder, SourceBuilder)
+    RequestBuilder.source()(api_key=VALID_KEY, source_id=VALID_SOURCE_ID)
 
 
 @pytest.mark.unit_test
